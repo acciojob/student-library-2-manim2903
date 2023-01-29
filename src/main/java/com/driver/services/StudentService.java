fd5a91492bfc4fe9f424ee7b3845aaedb99dccc2
mani <mani>
2023-01-29 13:13:23 +0530

    commit

--- src/main/java/com/driver/services/StudentService.java
+++ src/main/java/com/driver/services/StudentService.java
@@ -1,6 +1,8 @@
 package com.driver.services;
 
+import com.driver.models.Card;
 import com.driver.models.Student;
+import com.driver.repositories.CardRepository;
 import com.driver.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,28 +16,35 @@ public class StudentService {
 
     @Autowired
     StudentRepository studentRepository4;
+    @Autowired
+    private CardRepository cardRepository;
 
     public Student getDetailsByEmail(String email){
-        Student student = null;
+        Student student = studentRepository4.findByEmailId(email);
 
         return student;
     }
 
     public Student getDetailsById(int id){
-        Student student = null;
+        Student student = studentRepository4.findById(id).get();
 
         return student;
     }
 
     public void createStudent(Student student){
-
+        studentRepository4.save(student);
     }
 
     public void updateStudent(Student student){
-
+        studentRepository4.save(student);
     }
 
     public void deleteStudent(int id){
         //Delete student and deactivate corresponding card
+        Card card=cardRepository.findById(id).get();
+        Student student=card.getStudent();
+        cardRepository.delete(card);
+        studentRepository4.delete(student);
+
     }
 }
